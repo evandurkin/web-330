b81cfacbbe546995d8c9278fbe779e155b40cdc0
Evan Durkin <edurkin@my365.bellevue.edu>
2021-08-28 20:59:37 -0400

    adding week-3 files - not completed

---
+++ week-3/index.js
@@ -0,0 +1,7 @@
+//exporting all of the JS classes to index file
+
+export * from "./appetizer.js";
+export * from "./beverage.js";
+export * from "./dessert.js";
+export * from "./main-course.js";
+export * from "./bill.js";
