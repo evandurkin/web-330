0843da56e4a0e4683a032da65185f93a4cc236c4
Evan Durkin <edurkin@my365.bellevue.edu>
2021-10-10 23:34:40 -0400

    personal portfolio updates

--- personal-portfolio/footer.js
+++ personal-portfolio/footer.js
@@ -9,13 +9,16 @@ class Footer extends HTMLElement
 
     connectedCallback() {
         this.innerHTML = 
+        // <a href="./index.html"><img src="./logo/durkin-logo-index.png" alt="" class="logo" style="margin-top: 16px;"></a>
             `<ul>
                 <li><p style="font-weight:bold; margin-bottom:10px;">Links</p></li>
-                <li><a href="">GitHub</a></li>
-                <li><a href="">BU Web Dev Degree</a></li>
-                <li><a href="">BU YouTube</a></li>
-                <li><a href="">BU GitHub</a></li>
+                <li><a href="https://github.com/evandurkin">GitHub</a></li>
+                <li><a href="https://www.bellevue.edu/degrees/bachelor/web-development-bs/">BU Web Dev Degree</a></li>
+                <li><a href="https://www.youtube.com/user/bellevueuniversity">BU YouTube</a></li>
+                <li><a href="https://github.com/buwebdev">BU GitHub</a></li>
             </ul>`
+            // <h2><a href="#top" id="back-to-top">Top</a></h2>
+     
     }
 }
 
